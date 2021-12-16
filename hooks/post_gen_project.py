26e2a8e05f080a6228e866aba0e91424773af942
wtong2017 <wtong@connect.ust.hk>
2021-12-16 15:48:14 +0800

    feat: add web crawler requirement and set up git hooks

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -20,3 +20,6 @@ if __name__ == '__main__':
 
     if 'Not open source' == '{{ cookiecutter.open_source_license }}':
         remove_file('LICENSE')
+
+    if 'n' == '{{ cookiecutter.use_pre_commit }}':
+        remove_file('.pre-commit-config.yaml')
